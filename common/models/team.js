717fd98f0ef5a981564901acafee82869cea74b3
Arturo Filastò <art@fuffa.org>
2015-07-22 14:46:37 +0200

    Start sketching out how the API should look like and add TeamMember model

--- common/models/team.js
+++ common/models/team.js
@@ -1,3 +1,34 @@
 module.exports = function(Team) {
+  Team.join = function(team_id, cb) {
+    var response = [
+      "member1",
+      "member2"
+    ];
+    cb(null, response);
+  }
 
+  Team.leave = function(team_id, cb) {
+    var response = [
+      "member1"
+    ];
+    cb(null, response);
+  }
+
+  Team.remoteMethod(
+    'leave',
+    {
+      http: {path: '/leave', verb: 'post'},
+      accepts: {arg: 'id', type: 'number', http: { source: 'query' } },
+      returns: {arg: 'members', type: ['string']}
+    }
+  )
+
+  Team.remoteMethod(
+    'join',
+    {
+      http: {path: '/join', verb: 'post'},
+      accepts: {arg: 'id', type: 'number', http: { source: 'query' } },
+      returns: {arg: 'members', type: ['string']}
+    }
+  )
 };
