09bf871233a25926aeec106511026f1d5162c3ff
Arturo Filastò <art@fuffa.org>
2015-07-30 22:35:31 +0200

    Implement mechanism for getting DB from env variables
    * Start outlining the mapping between teams and oonitarians
    * Write some dummy data

--- common/models/team.js
+++ common/models/team.js
@@ -1,34 +1,71 @@
 module.exports = function(Team) {
-  Team.join = function(team_id, cb) {
-    var response = [
-      "member1",
-      "member2"
-    ];
-    cb(null, response);
-  }
+// {
+//   "username": "hellais",
+//   "email": "accounts@fuffa.org",
+//   "password": "antani"
+// }
 
-  Team.leave = function(team_id, cb) {
-    var response = [
-      "member1"
-    ];
-    cb(null, response);
-  }
+  // Team.join = function(team_id, cb) {
+  //   var response = [
+  //     "member1",
+  //     "member2"
+  //   ];
+  //   cb(null, response);
+  // }
+  // Team.remoteMethod(
+  //   'join',
+  //   {
+  //     http: {path: '/join', verb: 'post'},
+  //     accepts: {arg: 'id', type: 'number', http: { source: 'query' } },
+  //     returns: {arg: 'members', type: ['string']}
+  //   }
+  // )
+  // Team.create = function(cb) {
+  //   var response = [
+  //     "member1"
+  //   ];
+  //   cb(null, response);
+  // }
 
-  Team.remoteMethod(
-    'leave',
-    {
-      http: {path: '/leave', verb: 'post'},
-      accepts: {arg: 'id', type: 'number', http: { source: 'query' } },
-      returns: {arg: 'members', type: ['string']}
-    }
-  )
+  // Team.remoteMethod(
+  //   'create',
+  //   {
+  //     http: {path: '/create', verb: 'post'},
+  //     accepts: {arg: 'id', type: 'number', http: { source: 'query' } },
+  //     returns: {arg: 'members', type: ['string']}
+  //   }
+  // )
+  // Team.leave = function(team_id, cb) {
+  //   var response = [
+  //     "member1"
+  //   ];
+  //   cb(null, response);
+  // }
 
-  Team.remoteMethod(
-    'join',
-    {
-      http: {path: '/join', verb: 'post'},
-      accepts: {arg: 'id', type: 'number', http: { source: 'query' } },
-      returns: {arg: 'members', type: ['string']}
-    }
-  )
+  // Team.remoteMethod(
+  //   'leave',
+  //   {
+  //     http: {path: '/leave', verb: 'post'},
+  //     accepts: {arg: 'id', type: 'number', http: { source: 'query' } },
+  //     returns: {arg: 'members', type: ['string']}
+  //   }
+  // )
+
+  // Team.listTeams = function(cb) {
+  //   var response = [
+  //     {"name": "teamx"}
+  //   ];
+  //   cb(null, response);
+  // }
+
+  // Team.remoteMethod(
+  //   'listTeams',
+  //   {
+  //     http: {path: '/list', verb: 'get'},
+  //     accepts: {},
+  //     returns: {arg: 'teams', type: ['object']}
+  //   }
+  // )
+
+
 };
